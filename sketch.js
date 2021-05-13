a3df785f99e6533e4a547eb4ed119ceb6937f5b6
ahona28 <77285013+ahona28@users.noreply.github.com>
2021-05-13 20:43:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -43,20 +43,39 @@ function setup() {
   //top
   block16 = new Block(390,155,30,40);
 
+  //set 2 for second stand
+  //level one
+  blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40);
+  blocks3 = new Block(700,175,30,40);
+  blocks4 = new Block(730,175,30,40);
+  blocks5 = new Block(760,175,30,40);
+  //level two
+  blocks6 = new Block(670,135,30,40);
+  blocks7 = new Block(700,135,30,40);
+  blocks8 = new Block(730,135,30,40);
+  //top
+  blocks9 = new Block(700,95,30,40);
 
-  
+  //ball holder with slings
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:100,y:200});
 
 }
 function draw() {
   background(56,44,44); 
  
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
   textSize(20);
   fill("lightyellow");
-  
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
 
   ground.display();
   stand1.display();
-  
+  stand2.display();
   strokeWeight(2);
   stroke(15);
   fill("skyblue");
@@ -79,6 +98,27 @@ function draw() {
   block15.display();
   fill("grey");
   block16.display();
- 
+  fill("skyblue");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("turquoise");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("pink")
+  blocks9.display();
+  fill("gold");
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
 
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
 }
